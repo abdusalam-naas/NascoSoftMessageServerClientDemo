1d2fee4e21021dea1ac59c787626dbfe7597212f
Abdusalam Naas <abdusalam.naas@gmail.com>
2022-10-11 14:00:05 +0200

    11-10-2022

--- MessageServerClientDemo/FrmSms.Designer.cs
+++ MessageServerClientDemo/FrmSms.Designer.cs
@@ -28,8 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.lblExpiry = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
@@ -42,26 +40,6 @@
             this.txtPhoneNo = new System.Windows.Forms.TextBox();
             this.btnSEndText = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(77, 47);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(88, 110);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // lblExpiry
             // 
@@ -180,8 +158,6 @@
             this.Controls.Add(this.txtPassword);
             this.Controls.Add(this.txtUserName);
             this.Controls.Add(this.btnAuthenticate);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
             this.Name = "FrmSms";
             this.Text = "FrmSignalR";
             this.ResumeLayout(false);
@@ -190,9 +166,6 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label lblExpiry;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
--- MessageServerClientDemo/FrmSms.cs
+++ MessageServerClientDemo/FrmSms.cs
@@ -12,60 +12,16 @@ namespace MessageServerClientDemo
         DateTime smsJwtexpirty;
         DateTime smsSubscriptionExpiry;
         const string smsApiServer = "https://msg.nascosoft.ly";
-        //const string smsApiServer = "http://192.168.1.50:5252";
-        HubConnection connection;
 
         public FrmSms()
         {
             InitializeComponent();
         }
 
-        
-        async void StartConnection()
-        {
-            var signalRUrl = $"{smsApiServer}/smshub";
-            connection = new HubConnectionBuilder()
-                .WithUrl(signalRUrl, options =>
-                {
-                    options.AccessTokenProvider = ()=> Task.FromResult(smsJwt);
-                })
-                .Build();
-            connection.On("SendClientInfo", () =>
-            {
-                connection.SendAsync("ReceiveClientInfo", "Abdusalam pc");
-            });
-            //connection.ServerTimeout = new TimeSpan(0, 0, 2);
-            try
-            {
-                await connection.StartAsync();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            StartConnection();
-        }
-
-        async private void button2_Click(object sender, EventArgs e)
-        {
-            await connection.StopAsync();
-        }
-
-        SmsClient CreateSmsClient(string jwt)
-        {
-            var smsapiurl = $"{smsApiServer}/api/sms/";
-            return new SmsClient(new Uri(smsapiurl), jwt);
-        }
-
-
         async private void btnAuthenticate_Click(object sender, EventArgs e)
         {
             Properties.Settings.Default.Save();
-            var client = CreateSmsClient(null);
+            var client = new SmsClient();
             var ar = await client.Authenticate(txtUserName.Text, txtPassword.Text);
 
             if (ar != null)
@@ -84,7 +40,7 @@ namespace MessageServerClientDemo
 
         async private void btnSEndText_Click(object sender, EventArgs e)
         {
-            var client = CreateSmsClient(smsJwt);
+            var client = new SmsClient(smsJwt);
             await client.SendTextAsync(txtPhoneNo.Text, txtMessage.Text);
         }
     }
